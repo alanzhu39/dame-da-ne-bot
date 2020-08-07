3b1649a77d3b6dd0f9a51411706aea2cb8de8ac8
alanzhu39 <36008118+alanzhu39@users.noreply.github.com>
2020-08-07 14:50:37 -0700

    some early testing

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
 import tweepy
+from os import environ
+
+credentials = open('credentials.txt', 'r')
+
+interval = 60
+
+consumer_key = credentials.readline().strip()
+consumer_secret = credentials.readline().strip()
+access_key = credentials.readline().strip()
+access_secret = credentials.readline().strip()
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+mentions = api.mentions_timeline()
+for status in mentions:
+    print(status.entities['hashtags'])
+    if 'DameDaNeBot' in status.entities['hashtags']:
+        print('success')
+"""
+while True:
+    mentions = api.mentions_timeline()
+    recents = [status.in_reply_to_status_id for status in api.user_timeline('DameDaNeMe')]
+    curr = None
+    for status in mentions:
+        if status.id not in recents:
+            curr = status
+            break
+    if curr == None:
+        continue
+    time.sleep(interval)
+"""
