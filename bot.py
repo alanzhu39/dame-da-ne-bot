7c12e128b5d15dcc15dbed41d10a55f6827fece2
Alan Zhu <alan.zhu39@gmail.com>
2020-08-14 14:27:17 -0700

    it works now pog

--- bot.py
+++ bot.py
@@ -15,45 +15,25 @@ access_secret = credentials.readline().strip()
 
 api = Twython(consumer_key, consumer_secret, access_key, access_secret)
 
-video = open('final.mp4', 'rb')
-response = api.upload_video(media=video, media_type='video/mp4')
-api.update_status(status="Here is your personally generated meme! I am a bot.", media_ids=[response['media_id']])
-video.close()
-
-mentions = api.get_mentions_timeline()
-recents = [status['in_reply_to_status_id'] for status in api.get_user_timeline()]
-curr = None
-for status in mentions:
-    if status['id'] not in recents:
-        texts = [hashtag['text'] for hashtag in status['entities']['hashtags']]
-        if 'DameDaNeMe' in texts:
-            curr = status
-            break
-if curr is not None:
-    print('success')
-    print(curr['id'])
-    """
-    f = open('files/02.png', 'wb')
-    f.write(requests.get(curr['user']['profile_image_url_https']).content)
-    f.close()
-    makevid()
-    video = open('final.mp4', 'rb')
-    response = api.upload_video(media=video, media_type='video/mp4', media_category='TweetVideo')
-    api.update_status(status="@{} Here is your personally generated meme! I am a bot.".format(curr['user']['screen_name']), media_ids=[response['media_id']], in_reply_to_status_id=curr['id'])
-    """
-
-"""
 while True:
-    mentions = api.mentions_timeline()
-    recents = [status.in_reply_to_status_id for status in api.user_timeline('DameDaNeBot')]
+    mentions = api.get_mentions_timeline()
+    recents = [status['in_reply_to_status_id'] for status in api.get_user_timeline()]
     curr = None
     for status in mentions:
-        if status.id not in recents:
-            texts = [hashtag['text'] for hashtag in status.entities['hashtags']]
+        if status['id'] not in recents:
+            texts = [hashtag['text'] for hashtag in status['entities']['hashtags']]
             if 'DameDaNeMe' in texts:
                 curr = status
                 break
     if curr is not None:
-
+        print('success')
+        print(curr['id'])
+        f = open('files/02.png', 'wb')
+        f.write(requests.get(curr['user']['profile_image_url_https']).content)
+        f.close()
+        makevid()
+        video = open('final.mp4', 'rb')
+        response = api.upload_video(media=video, media_type='video/mp4')
+        api.update_status(status="@{} Here is your personally generated meme! I am a bot.".format(curr['user']['screen_name']), media_ids=[response['media_id']], in_reply_to_status_id=curr['id'])
+    break
     time.sleep(interval)
-"""
--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-from moviepy.editor import *
+from twython import Twython
+import requests
+from io import BytesIO
+from makevid import makevid
+from os import environ
 
-generated = VideoFileClip('final.mp4')
-audioClip = AudioFileClip('damedane.mp3')
-generated = generated.set_audio(audioClip)
-generated.write_videofile('final.mp4', bitrate='256k')
+credentials = open('credentials.txt', 'r')
+
+interval = 60
+
+consumer_key = credentials.readline().strip()
+consumer_secret = credentials.readline().strip()
+access_key = credentials.readline().strip()
+access_secret = credentials.readline().strip()
+
+api = Twython(consumer_key, consumer_secret, access_key, access_secret)
+
+mentions = api.get_mentions_timeline()
+for mention in mentions:
+    print(mention['user']['screen_name'])
