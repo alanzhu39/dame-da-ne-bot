55dfd6e33ee11349086f489721d48490cb7e3f31
Alan Zhu <alan.zhu39@gmail.com>
2020-08-25 18:20:40 -0700

    running on mac

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from moviepy.editor import *
+
 from twython import Twython
 import requests
 from io import BytesIO
@@ -7,21 +7,20 @@ from os import environ
 
 credentials = open('credentials.txt', 'r')
 
-interval = 60
-
 consumer_key = credentials.readline().strip()
 consumer_secret = credentials.readline().strip()
 access_key = credentials.readline().strip()
 access_secret = credentials.readline().strip()
 
 api = Twython(consumer_key, consumer_secret, access_key, access_secret)
-"""
+
+f = open('files/02.png', 'wb')
+base = open('base.jpg', 'rb')
+f.write(base.read())
+f.close()
+base.close()
+makevid()
 video = open('final.mp4', 'rb')
 response = api.upload_video(media=video, media_type='video/mp4')
-api.update_status(status="Here is your personally generated meme! I am a bot.", media_ids=[response['media_id']])
+api.update_status(status="Making memes since 2020. Follow the instructions in bio to get your own Dame Da Ne meme like the one below!",media_ids=[response['media_id']])
 video.close()
-"""
-generated = VideoFileClip('final.mp4')
-audioClip = AudioFileClip('damedane.mp3')
-generated = generated.set_audio(audioClip)
-generated.write_videofile('final.mp4',audio_codec='aac')
